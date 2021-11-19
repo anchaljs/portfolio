e929009e6bd1ccbf4e71af96295c9176070c4304
Anchal <anchal18052001@gmail.com>
2021-11-19 22:19:32 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+$ (document) .ready(function(){
+ $(window).scroll(function(){
+     if(this.scrollY > 20){
+         $('.navbar').addClass("sticky");
+     }else{
+        $('.navbar').removeClass("sticky");
+     }
+     if(this.scroll>500){
+      $('.scroll-up-btn').addClass("show");
+     }else{
+      $('.scroll-up-btn').removeClass("show");
+
+     }
+ });
+ // slide up script
+ $('.scroll-up-btn').click(function(){
+   $('html').animated({scrllTop :0});
+ });
+// toogle menu / navbar script//
+$('.menu-btn').click(function(){
+  $('.navbar .menu').toggleClass("active");
+  $('.menu-btn i').toggleClass("active");
+})
+})
